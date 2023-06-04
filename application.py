60c51d13265626d94c388ae76c90f3cc58689af8
ayush rajgor <ayushrajgorar@gmail.com>
2023-06-04 13:24:32 +0530

    initial commit

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 
 application = Flask(__name__)
 
 
 @application.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
